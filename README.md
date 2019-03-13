23a6fac39f47be620deee0a0cb9440d1f5fb4fa2
SarzhLean <oleksandr@leanconvert.com>
2019-03-13 14:15:38 +0200

    MVP is done

---
+++ README.md
@@ -0,0 +1,12 @@
+## Overview
+
+Run local node server:
+```shell
+$ node index
+```
+
+Access example page:
+http://127.0.0.1:9000/src/html/index.html
+
+Original library:
+http://taitems.github.io/jQuery.Gantt/
---
+++ src/js/data.js
@@ -0,0 +1,241 @@
+var data = [{
+  name: "Sprint 0",
+  desc: "Description Sprint 0",
+  dataObj: {
+    budget: '3000.00',
+    owner: 'Superviser19',
+    contractor: 'Victori&Co'
+  },
+  values: [{
+    from: "2019-01-30",
+    to: "2019-02-06",
+    label: "Planned time UTC",
+    customClass: "ganttPlanned"
+  }, {
+    from: "2019-01-28",
+    to: "2019-02-04",
+    label: "Fact time UTC",
+    customClass: "ganttFact",
+    isLower: true
+  }]
+},{
+  name: "Sprint 1",
+  desc: "Description Sprint 1",
+  dataObj: {
+    budget: '3000.00',
+    owner: 'Superviser19',
+    contractor: 'Victori&Co'
+  },
+  values: [{
+    from: "2019-02-01",
+    to: "2019-02-12",
+    label: "Planned time UTC",
+    customClass: "ganttPlanned"
+  }, {
+    from: "2019-02-11",
+    to: "2019-02-16",
+    label: "Fact time UTC",
+    customClass: "ganttFact",
+    isLower: true
+  }]
+},{
+  name: "Sprint 2",
+  desc: "Description Sprint 2",
+  dataObj: {
+    budget: '3000.00',
+    owner: 'Superviser19',
+    contractor: 'Victori&Co'
+  },
+  values: [{
+    from: "2019-02-17",
+    to: "2019-03-29",
+    label: "Planned time UTC",
+    customClass: "ganttPlanned"
+  }, {
+    from: "2019-02-18",
+    to: "2019-02-18",
+    label: "Fact time UTC",
+    customClass: "ganttFact",
+    isLower: true
+  }]
+},{
+  name: "Release Stage",
+  desc: "Description Release Stage",
+  dataObj: {
+    budget: '3000.00',
+    owner: 'Superviser19',
+    contractor: 'Victori&Co'
+  },
+  values: [{
+    from: "2019-02-21",
+    to: "2019-03-13",
+    label: "Planned time UTC",
+    customClass: "ganttPlanned"
+  }, {
+    from: "2019-03-01",
+    to: "2019-03-10",
+    label: "Fact time UTC",
+    customClass: "ganttFact",
+    isLower: true
+  }]
+},{
+  name: "Warranty Period",
+  desc: "Description Warranty Period",
+  dataObj: {
+    budget: '3000.00',
+    owner: 'Superviser19',
+    contractor: 'Victori&Co'
+  },
+  values: [{
+    from: "2019-03-09",
+    to: "2019-03-15",
+    label: "Planned time UTC",
+    customClass: "ganttPlanned"
+  }, {
+    from: "2019-03-17",
+    to: "2019-03-21",
+    label: "Fact time UTC",
+    customClass: "ganttFact",
+    isLower: true
+  }]
+},{
+  name: "Warranty Period",
+  desc: "Description Warranty Period",
+  dataObj: {
+    budget: '3000.00',
+    owner: 'Superviser19',
+    contractor: 'Victori&Co'
+  },
+  values: [{
+    from: "2019-03-22",
+    to: "2019-04-15",
+    label: "Planned time UTC",
+    customClass: "ganttPlanned"
+  }, {
+    from: "2019-03-25",
+    to: "2019-04-21",
+    label: "Fact time UTC",
+    customClass: "ganttFact",
+    isLower: true
+  }]
+}, {
+  name: "Sprint 0",
+  desc: "Description Sprint 0",
+  dataObj: {
+    budget: '3000.00',
+    owner: 'Superviser19',
+    contractor: 'Victori&Co'
+  },
+  values: [{
+    from: "2019-01-30",
+    to: "2019-02-06",
+    label: "Planned time UTC",
+    customClass: "ganttPlanned"
+  }, {
+    from: "2019-01-28",
+    to: "2019-02-04",
+    label: "Fact time UTC",
+    customClass: "ganttFact",
+    isLower: true
+  }]
+},{
+  name: "Sprint 1",
+  desc: "Description Sprint 1",
+  dataObj: {
+    budget: '3000.00',
+    owner: 'Superviser19',
+    contractor: 'Victori&Co'
+  },
+  values: [{
+    from: "2019-02-01",
+    to: "2019-02-12",
+    label: "Planned time UTC",
+    customClass: "ganttPlanned"
+  }, {
+    from: "2019-02-11",
+    to: "2019-02-16",
+    label: "Fact time UTC",
+    customClass: "ganttFact",
+    isLower: true
+  }]
+},{
+  name: "Sprint 2",
+  desc: "Description Sprint 2",
+  dataObj: {
+    budget: '3000.00',
+    owner: 'Superviser19',
+    contractor: 'Victori&Co'
+  },
+  values: [{
+    from: "2019-02-17",
+    to: "2019-03-29",
+    label: "Planned time UTC",
+    customClass: "ganttPlanned"
+  }, {
+    from: "2019-02-18",
+    to: "2019-02-18",
+    label: "Fact time UTC",
+    customClass: "ganttFact",
+    isLower: true
+  }]
+},{
+  name: "Release Stage",
+  desc: "Description Release Stage",
+  dataObj: {
+    budget: '3000.00',
+    owner: 'Superviser19',
+    contractor: 'Victori&Co'
+  },
+  values: [{
+    from: "2019-02-21",
+    to: "2019-03-13",
+    label: "Planned time UTC",
+    customClass: "ganttPlanned"
+  }, {
+    from: "2019-03-01",
+    to: "2019-03-10",
+    label: "Fact time UTC",
+    customClass: "ganttFact",
+    isLower: true
+  }]
+},{
+  name: "Warranty Period",
+  desc: "Description Warranty Period",
+  dataObj: {
+    budget: '3000.00',
+    owner: 'Superviser19',
+    contractor: 'Victori&Co'
+  },
+  values: [{
+    from: "2019-03-09",
+    to: "2019-03-15",
+    label: "Planned time UTC",
+    customClass: "ganttPlanned"
+  }, {
+    from: "2019-03-17",
+    to: "2019-03-21",
+    label: "Fact time UTC",
+    customClass: "ganttFact",
+    isLower: true
+  }]
+},{
+  name: "Warranty Period",
+  desc: "Description Warranty Period",
+  dataObj: {
+    budget: '3000.00',
+    owner: 'Superviser19',
+    contractor: 'Victori&Co'
+  },
+  values: [{
+    from: "2019-03-22",
+    to: "2019-04-15",
+    label: "Planned time UTC",
+    customClass: "ganttPlanned"
+  }, {
+    from: "2019-03-25",
+    to: "2019-04-21",
+    label: "Fact time UTC",
+    customClass: "ganttFact",
+    isLower: true
+  }]
+}];
